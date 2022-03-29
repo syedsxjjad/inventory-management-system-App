0d980a0153f37dd7a8b8f4bc76b469811039307c
syedsxjjad <syedsxjjad@gmail.com>
2022-03-29 21:10:44 +0500

    implement_Functionality

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+// import { collection, onSnapshot } from 'firebase/firestore';
+import React, { useEffect } from 'react';
+import { db } from '../Firebase/Firebase';
 import { UserContext } from './Context' 
+
+import {
+  updateDoc,
+  deleteDoc,
+  collection,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
 
 export const UsersData: React.FC = (props): React.ReactElement => {
   const [productView, setProductView] = React.useState([]);
+  useEffect(() => {
+    onSnapshot(collection(db, "Inventory-Management"), (snapshoot:any) => {
+      setProductView(
+        snapshoot.docs.map((doc:any) => {
+          //   console.log(doc);
+          return {
+            ...doc.data(),
+            id: doc.id,
+          };
+        })
+      );
+    });
+  }, []);
 
 
 //   const [token, setToken] = React.useState<string | null>('');
--- src/screen/Products/Products.tsx
+++ src/screen/Products/Products.tsx
@@ -3,27 +3,35 @@ import { Link } from "react-router-dom";
 import ViewProduct from "./ViewProduct";
 import AddProduct from "./AddProduct";
 import Button from "../../component/Button";
+import DropDown from "../../component/DropDown";
 
 
 const Products = () => {
   return (
     <>
+   
+   {/* <div className=" h-auto"> */}
+     <div className="ml-96 flex justify-between  ">
       <div
-        className=" w-9/12 h-auto m-auto ml-80 mt-24"
+        className=" h-36 w-[70rem] fixed bg-white  py-16 mt-12"
       >
         <Link to="/AddProduct">
           <Button
           title="Add Product"
           />
-          {/* <div className="w-40 h-14 bg-slate-900 rounded-md li  max-w-xs hover:scale-110 transition duration-200 ease-in-out">
-            <img className="w-10 h-10 ml-14  " src="add.png" />
-         
-            <span className="text-white ml-24 ">Add Product</span>
-          </div> */}
         </Link>
-        <div className="w-auto h-auto"></div>
-        <ViewProduct />
+        </div>
+        <div className="absolute  ml-[52rem]  mt-24 ">
+      <DropDown />
+   
       </div>
+      </div>
+        <div className=" w-96 h-screen mt-48 mr-60 ">
+        <ViewProduct /> 
+          </div> 
+     
+    {/* </div> */}
+    
     </>
   );
 };
